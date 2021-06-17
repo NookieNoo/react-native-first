978f44e49e9fcdb22e21ca8516124540950e23f2
NookieNoo <biv0208@yandex.ru>
2021-06-17 15:52:29 +0300

    Стилизация

--- src/screens/TodoScreen.js
+++ src/screens/TodoScreen.js
@@ -1,14 +1,22 @@
 import React from "react";
 import { StyleSheet, Text, View, Button } from "react-native";
 import { THEME } from "../theme";
+import AppCard from "../components/ui/AppCard";
 
 export default function TodoScreen({ goBack, todo }) {
     return (
         <View>
-            <Text>{todo.title}</Text>
+            <AppCard style={styles.card}>
+                <Text style={styles.title}>{todo.title}</Text>
+                <Button title="Ред." />
+            </AppCard>
             <View style={styles.buttons}>
                 <View style={styles.button}>
-                    <Button title="Назад" color={THEME.GREY_COLOR} onPress={goBack} />
+                    <Button
+                        title="Назад"
+                        color={THEME.GREY_COLOR}
+                        onPress={goBack}
+                    />
                 </View>
                 <View style={styles.button}>
                     <Button
@@ -27,7 +35,14 @@ const styles = StyleSheet.create({
         flexDirection: "row",
         justifyContent: "space-between",
     },
+    card: {
+        marginBottom: 20,
+        padding: 15
+    },
     button: {
-        width: '40%',
+        width: "40%",
+    },
+    title: {
+        fontSize: 20
     }
 });
